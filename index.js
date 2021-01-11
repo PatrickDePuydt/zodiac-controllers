ee7c5d07d635d7907bc382c9bceaaea307ccde8a
PatrickDePuydt <prdepuydt@gmail.com>
2021-01-11 11:57:41 -0700

    master: Add other elements

--- index.js
+++ index.js
@@ -14,4 +14,16 @@ app.get('/air', (req, res)=>{
   res.render('./air');
 });
 
+app.get('/water', (req, res)=>{
+  res.render('./water');
+});
+
+app.get('/fire', (req, res)=>{
+  res.render('./fire');
+});
+
+app.get('/earth', (req, res)=>{
+  res.render('./earth');
+});
+
 app.listen(3000);
